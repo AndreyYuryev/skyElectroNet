0bd488a8921df99a2b3fd6c898c1ebe59bead87e
Andrey <yuryev@yandex.ru>
2024-05-24 00:57:59 +0300

    изменение доступа к модели User

--- electronet/admin.py
+++ electronet/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# actions = ["clear_debt", ]
+#
+#
+# @admin.action(description="Очистить задолженность перед поставщиком")
+# def clear_debt(self, request, queryset):
+#     # queryset.update(status='published')
+#     pass
